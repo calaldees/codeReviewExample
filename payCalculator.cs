8f3a6bd2dd4d10e3575f720888e495223afec60c
Allan Callaghan <calaldees@gmail.com>
2020-10-15 16:16:04 +0100

    Added my new pay calculator It can be modified to take different numbers of hours and rate of pay

---
+++ payCalculator.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace MyFirstApplication
+{
+    static void Main(string[] args)
+    {																			
+            Console.WriteLine("You worked 40 hours");
+            Console.WriteLine("Your payrate is 534p per hour");
+
+            Console.Write("Your Gross Pay is ");
+            Console.WriteLine(40 * 534);
+            Console.Write("Deductions are ");
+            Console.WriteLine(4000+1000);
+            Console.Write("Your Gross Pay is");
+            Console.WriteLine((40*534) -(4000+1000));
+
+			Console.ReadKey();
+		}
+	}
+}
